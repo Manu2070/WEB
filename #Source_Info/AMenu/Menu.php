f12f4005de7e7dd2ff37654426f1a0819e624096
Kevin <Raffaello@hotmail.de>
2019-07-26 14:24:01 +0200

    Fix

---
+++ #Source_Info/AMenu/Menu.php
@@ -0,0 +1,33 @@
+   <link rel="stylesheet" href="style.css">
+    <div class="middle">
+      <div class="menu">
+        <li class="item" id='profile'>
+          <a href="#profile" class="btn"><i class="far fa-user"></i>Profile</a>
+          <div class="smenu">
+            <a href="#">Posts</a>
+            <a href="#">Picture</a>
+          </div>
+        </li>
+
+        <li class="item" id="messages">
+          <a href="#messages" class="btn"><i class="far fa-envelope"></i>Messages</a>
+          <div class="smenu">
+            <a href="#">new</a>
+            <a href="#">Sent</a>
+            <a href="#">Spam</a>
+          </div>
+        </li>
+
+        <li class="item" id="settings">
+          <a href="#settings" class="btn"><i class="fas fa-cog"></i>Settings</a>
+          <div class="smenu">
+            <a href="#">Password</a>
+            <a href="#">Language</a>
+          </div>
+        </li>
+
+        <li class="item">
+          <a class="btn" href="#"><i class="fas fa-sign-out-alt"></i>Logout</a>
+        </li>
+      </div>
+    </div>
